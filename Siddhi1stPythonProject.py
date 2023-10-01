975365ffbee0b92d6c749e04f395730efc4866b4
alok080205 <alok080205@gmail.com>
2023-10-01 18:19:31 +0530

    adding changes to feature1

--- Siddhi1stPythonProject.py
+++ Siddhi1stPythonProject.py
@@ -1,3 +1,3 @@
 print("this is 1st python project").
 print("this is 1st python project and main branch").
-print("this is 1st python project and feature1 branch").
+print("this is 1st python project and feature1 branch and we are making some changes to existing code in feature1").
